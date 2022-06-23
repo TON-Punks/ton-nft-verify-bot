55299acb2da72a43784850c3229d5740a1e01227
thepaket <101527506+thepaket@users.noreply.github.com>
2022-06-23 09:46:12 +0300

    get_user_nfts function update

--- functions.py
+++ functions.py
@@ -44,26 +44,21 @@ async def get_ton_addresses(address):
 
 
 async def get_user_nfts(address):
-    address = detect_address(address)['bounceable']['b64url']
+    address = (await get_ton_addresses(address))['b64url']
     await asyncio.sleep(1)
+    all_nfts = json.loads(requests.get(f"https://tonapi.io/v1/nft/getItemsByOwnerAddress",
+                                       params={"owner": address,
+                                               "collection": COLLECTION,
+                                               "include_on_sale": "true",
+                                               "limit": "50",
+                                               "offset": 0}).text)['nft_items']
     nfts = []
-    all_nfts = json.loads(requests.get(f"https://tonapi.io/v1/nft/getItemsByOwnerAddress?account={address}").text)['nft_items']
-    await asyncio.sleep(1)
-    sell_nfts = json.loads(requests.get(f"https://tonapi.io/v1/nft/getNftForSale?account={address}").text)
-    for market in sell_nfts:
-        all_nfts += sell_nfts[market]
     for nft in all_nfts:
         try:
-            nft = nft['nft']
-        except:
-            pass
-        try:
-            collection = nft['collection_address']
-            if (await get_ton_addresses(collection))['b64url'] != COLLECTION:
-                name = nft['metadata']['name']
-                address = detect_address(nft['address'])['bounceable']['b64url']
-                image = nft['metadata']['image']
-                nfts += [{'address': address, 'name': name, 'image': image}]
+            name = nft['metadata']['name']
+            address = (await get_ton_addresses(nft['address']))['b64url']
+            image = nft['metadata']['image']
+            nfts += [{'address': address, 'name': name, 'image': image}]
         except:
             pass
     return nfts
